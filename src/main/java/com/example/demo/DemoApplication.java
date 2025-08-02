d83836b2f3cf82351add230d8047518a28213a8e
vismaypatel30 <vismaypatel30@gmail.com>
2025-08-02 23:00:21 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+ @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
 
 }
