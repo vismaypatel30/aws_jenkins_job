9cd2bbc20bebf4225b35c01a11b5cd6eb0791554
vismaypatel30 <vismaypatel30@gmail.com>
2025-08-03 00:00:16 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+    void testSomething() {
+        assertTrue(true);
+    }
 
 }
