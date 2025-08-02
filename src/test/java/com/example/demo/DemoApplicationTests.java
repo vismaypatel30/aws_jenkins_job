ef7065f85097d313751b2a1ac0bb5cd099dea788
vismaypatel30 <vismaypatel30@gmail.com>
2025-08-03 00:10:39 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue; // ✅ IMPORTANT IMPORT
+
 
 @SpringBootTest
 class DemoApplicationTests {
